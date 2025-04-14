45a045dcf358e0de79e7ebba2400d7cf451120ac
serious-joker-crypto <alexkabuthi6@gmail.com>
2025-04-14 18:53:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "serious~joker~bot",
   botOwner: 'serious-joker-crypto', // Your name
   ownerNumber: '+254785787612', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "4.0.0",
 };
 
 module.exports = settings;
